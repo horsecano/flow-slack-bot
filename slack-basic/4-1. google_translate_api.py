da7a43aaf471e6cdd91d2fa4049f7b1c7ac18bb6
horsecano <a01049149446@gmail.com>
2024-08-16 17:40:42 +0900

    first commit

--- slack-basic/4-1. google_translate_api.py
+++
@@ -1,8 +0,0 @@
-import googletrans # pip install googletrans==4.0.0-rc1
-
-translator = googletrans.Translator()
-
-example = "How are you today?"
-result = translator.translate(example, dest='fr')
-
-print(f"How are you today? => {result.text}")
